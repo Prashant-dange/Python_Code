7cb271e4cd7fc2338634dfdfd49877667f9e9a91
Prashant Dange <prashantdange99@gmail.com>
2024-10-10 18:13:56 +0530

    feat:add two prog

---
+++ Python_Basic_code/002_Addition_of_two_numbers.py
@@ -0,0 +1,11 @@
+print("start of the program")
+import sys
+
+num1 = int(input("Enter a first number : "))
+num2 = int(input("Enter a second number : "))
+
+num3 = num1 + num2
+
+print("Addition of two numbers is : ",num3)
+
+print("End of program ")
