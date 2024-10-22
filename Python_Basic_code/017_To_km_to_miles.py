2ccf37382529c81d93229cdcb6dd469e1ecef137
Prashant Dange <prashantdange99@gmail.com>
2024-10-22 10:10:20 +0530

    feat:

---
+++ Python_Basic_code/017_To_km_to_miles.py
@@ -0,0 +1,9 @@
+print("Start of program")
+
+km= float(input("Enter a Km to convert into miles : "))
+
+miles = km/1.609
+
+print(km," convertion into miles is ",miles)
+
+print("End of program")
---
+++ Python_Basic_code/018_To_convert_celsius_to_fahrenheit.py
@@ -0,0 +1,9 @@
+print("Start of program")
+
+celcious = float(input("Enter celcious to convert into fahrenheit "))
+
+fahrenheit= (celcious*9/5)+32
+
+print(celcious," after converted into fahrenheit is ",fahrenheit)
+
+print("End of program")
