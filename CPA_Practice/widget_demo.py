6cdf01558a97af27bd5e1685253f3a82cd5e0544
Prashant Dange <prashantdange99@gmail.com>
2026-04-29 00:18:31 +0530

    added new code

--- CPA_Practice/widget_demo.py
+++ CPA_Practice/widget_demo.py
@@ -30,7 +30,7 @@ def main():
     root_window=tkinter.Tk()
     root_window.title("Test window")
 
-    master_frame=tkinter.ttk.Frame(root_window,padding="3 3 12 12")
+    master_frame=tkinter.ttk.Frame(root_window,padding="3  3 12 12")
     master_frame.grid(row=5,column=5,sticky=(tkinter.N,tkinter.W,tkinter.E,tkinter.S))
 
     B=tkinter.Button(master_frame)
